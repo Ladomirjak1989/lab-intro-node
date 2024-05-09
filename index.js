ea2732a4bf72c3a1b159c8647b7a0e70dfb90f12
Bettina <bettike18989@gmail.com>
2024-05-09 10:03:10 +0200

    lab-intro-node-done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() { 
+  this.items = []
+  this.length = 0
+}
+  add(item) {
+     // Find the index where the new item should be inserted to maintain sorted order
+     let index = 0;
+     while (index < this.length && this.items[index] < item) {
+       index++;
+     }
+ 
+     // Insert the item at the correct index
+     this.items.splice(index, 0, item);
+ 
+     // Update the length property
+     this.length++;
+   }
+  
 
-  add(item) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length - 1];
+  }
 
-  max() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  min() {}
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((acc, curr) => acc + curr, 0);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
